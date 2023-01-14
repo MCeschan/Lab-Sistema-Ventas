37cbfe83e3ddd92b286accc05d42d390a9b91f57
Martina <martinaceschan@gmail.com>
2023-01-14 13:56:56 -0300

    Agregar archivos de proyecto.

---
+++ WindowsApp/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WindowsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrearProducto = new System.Windows.Forms.Button();
+            this.btnCrearVendedor = new System.Windows.Forms.Button();
+            this.btnCrearEmpresa = new System.Windows.Forms.Button();
+            this.btnCrearFactura = new System.Windows.Forms.Button();
+            this.btnCrearRemito = new System.Windows.Forms.Button();
+            this.btnCrearCliente = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCrearProducto
+            // 
+            this.btnCrearProducto.Location = new System.Drawing.Point(287, 51);
+            this.btnCrearProducto.Name = "btnCrearProducto";
+            this.btnCrearProducto.Size = new System.Drawing.Size(181, 49);
+            this.btnCrearProducto.TabIndex = 0;
+            this.btnCrearProducto.Text = "Crear producto";
+            this.btnCrearProducto.UseVisualStyleBackColor = true;
+            this.btnCrearProducto.Click += new System.EventHandler(this.btnCrearProducto_Click);
+            // 
+            // btnCrearVendedor
+            // 
+            this.btnCrearVendedor.Location = new System.Drawing.Point(287, 159);
+            this.btnCrearVendedor.Name = "btnCrearVendedor";
+            this.btnCrearVendedor.Size = new System.Drawing.Size(181, 44);
+            this.btnCrearVendedor.TabIndex = 2;
+            this.btnCrearVendedor.Text = "Crear vendedor";
+            this.btnCrearVendedor.UseVisualStyleBackColor = true;
+            this.btnCrearVendedor.Click += new System.EventHandler(this.btnCrearVendedor_Click);
+            // 
+            // btnCrearEmpresa
+            // 
+            this.btnCrearEmpresa.Location = new System.Drawing.Point(287, 209);
+            this.btnCrearEmpresa.Name = "btnCrearEmpresa";
+            this.btnCrearEmpresa.Size = new System.Drawing.Size(181, 39);
+            this.btnCrearEmpresa.TabIndex = 3;
+            this.btnCrearEmpresa.Text = "Crear empresa";
+            this.btnCrearEmpresa.UseVisualStyleBackColor = true;
+            this.btnCrearEmpresa.Click += new System.EventHandler(this.btnCrearEmpresa_Click);
+            // 
+            // btnCrearFactura
+            // 
+            this.btnCrearFactura.Location = new System.Drawing.Point(287, 263);
+            this.btnCrearFactura.Name = "btnCrearFactura";
+            this.btnCrearFactura.Size = new System.Drawing.Size(181, 39);
+            this.btnCrearFactura.TabIndex = 4;
+            this.btnCrearFactura.Text = "Crear factura";
+            this.btnCrearFactura.UseVisualStyleBackColor = true;
+            this.btnCrearFactura.Click += new System.EventHandler(this.btnCrearFactura_Click);
+            // 
+            // btnCrearRemito
+            // 
+            this.btnCrearRemito.Location = new System.Drawing.Point(287, 320);
+            this.btnCrearRemito.Name = "btnCrearRemito";
+            this.btnCrearRemito.Size = new System.Drawing.Size(181, 40);
+            this.btnCrearRemito.TabIndex = 5;
+            this.btnCrearRemito.Text = "Crear remito";
+            this.btnCrearRemito.UseVisualStyleBackColor = true;
+            this.btnCrearRemito.Click += new System.EventHandler(this.btnCrearRemito_Click);
+            // 
+            // btnCrearCliente
+            // 
+            this.btnCrearCliente.Location = new System.Drawing.Point(287, 106);
+            this.btnCrearCliente.Name = "btnCrearCliente";
+            this.btnCrearCliente.Size = new System.Drawing.Size(181, 47);
+            this.btnCrearCliente.TabIndex = 6;
+            this.btnCrearCliente.Text = "Crear cliente";
+            this.btnCrearCliente.UseVisualStyleBackColor = true;
+            this.btnCrearCliente.Click += new System.EventHandler(this.btnCrearCliente_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCrearCliente);
+            this.Controls.Add(this.btnCrearRemito);
+            this.Controls.Add(this.btnCrearFactura);
+            this.Controls.Add(this.btnCrearEmpresa);
+            this.Controls.Add(this.btnCrearVendedor);
+            this.Controls.Add(this.btnCrearProducto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrearProducto;
+        private System.Windows.Forms.Button btnCrearVendedor;
+        private System.Windows.Forms.Button btnCrearEmpresa;
+        private System.Windows.Forms.Button btnCrearFactura;
+        private System.Windows.Forms.Button btnCrearRemito;
+        private System.Windows.Forms.Button btnCrearCliente;
+    }
+}
+
---
+++ WindowsApp/Form1.cs
@@ -0,0 +1,68 @@
+﻿using LibreríaEntidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrearProducto_Click(object sender, EventArgs e)
+        {
+            Producto uno = new Producto("Auto", "Auto azul usado", 80000, 0.8, 0.21, "Automotores Fiat", "Vehículos", "Autos");
+
+            MessageBox.Show("Producto:" + " " + uno.Nombre + ", " + uno.Descripcion + ", " + "el precio costo es " + uno.PrecioCosto + ", " +
+                "el margen es " + uno.Margen + ", " + "el IVA es " + uno.IVA + ", " + uno.Proveedor + ", " + "categoría: " + uno.Categoria + ", " + uno.SubCategoria + ". " +
+                "El PRECIO BRUTO es " + uno.PrecioBruto + ", " + "y el precio de VENTA es " + uno.PrecioVenta);
+
+        }
+
+        private void btnCrearCliente_Click(object sender, EventArgs e)
+        {
+            ClienteIndividuo newClient = new ClienteIndividuo("Martina", "Ceschan", "27-41185856-7", "martinaceschan@gmail.com", "2953410529", "Bahía Blanca, Buenos Aires");
+
+            MessageBox.Show("Cliente:" + " " + newClient.Nombre + " " + newClient.Apellido + ", " + "CUIT: " + newClient.CUIT + ", " + "EMAIL: " +
+                newClient.Email + ", " + "TELEFONO: " + newClient.Telefono + ", " + "DIRECCIÓN: " + newClient.Direccion);
+
+        }
+
+        private void btnCrearVendedor_Click(object sender, EventArgs e)
+        {
+            Vendedor newVendedor = new Vendedor("Laura", "López", "40256897", "laura@gmail.com", "2953568911", "Córdoba");
+
+            MessageBox.Show("Vendedor: " + newVendedor.Nombre + " " + newVendedor.Apellido + ", DNI: " + newVendedor.DNI + ", EMAIL: " + newVendedor.Email + ", TELEFONO: " + newVendedor.Telefono + ", DIRECCION: " + newVendedor.Direccion);
+        }
+
+        private void btnCrearEmpresa_Click(object sender, EventArgs e)
+        {
+            Empresa newEmpresa = new Empresa("Automotores Fiat", "21-25698-7", "Alberto Gómez", "alberto@gmail.com", "295342568", "Córdoba");
+            MessageBox.Show("Empresa: " + newEmpresa.Nombre + ", CUIT: " + newEmpresa.CUIT + ", CONTACTO: " + newEmpresa.Contacto + ", EMAIL: " + newEmpresa.Email + ", TELEFONO: " + newEmpresa.Telefono + ", DIRECCION: " + newEmpresa.Direccion);
+
+        }
+
+        private void btnCrearFactura_Click(object sender, EventArgs e)
+        {
+            Factura newFactura = new Factura("A", "589647-12", DateTime.Now, "Carlos", "Corrientes", "Consumidor final", "AA", "Venta automovil usado azul", 800000);
+
+            MessageBox.Show("Factura tipo: " + newFactura.Tipo + ", Numero: " + newFactura.Numero + ", FECHA: " + newFactura.Fecha.ToString() + ", CLIENTE: " + newFactura.Cliente + ", DIRECCIÓN: " + newFactura.Direccion + ", CondicionIVA: " + newFactura.CondicionIVA + ", Condicion Venta: " + newFactura.CondicionVenta + ", DETALLE: " + newFactura.Detalle + ", TOTAL: " + newFactura.Total);
+        }
+
+        private void btnCrearRemito_Click(object sender, EventArgs e)
+        {
+            Remito newRemito = new Remito("566625-7", DateTime.Now, "Carlos", "Corrientes", "Consumidor final", "AA", "Venta automovil usado azul", DateTime.Now, 800000);
+
+            MessageBox.Show("Remito Numero: " + newRemito.Numero + ", FECHA: " + newRemito.Fecha.ToString() + ", CLIENTE: " + newRemito.Cliente + ", DIRECCIÓN: " + newRemito.Direccion + ", CondicionIVA: " + newRemito.CondicionIVA + ", Condicion Venta: " + newRemito.CondicionVenta + ", DETALLE: " + newRemito.Detalle + ", FECHA DE ENTREGA: " + newRemito.FechaEntrega.ToString() + ", TOTAL: " + newRemito.Total);
+        }
+    }
+}
